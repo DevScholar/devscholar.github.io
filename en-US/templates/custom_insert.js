e3fc2e70efdbbaff1a0d11a15bc90a4b3d1b4466
DevScholar <88761139+DevScholar@users.noreply.github.com>
2024-06-28 22:25:48 +0800

    add experimental sidebar to the about page

--- en-US/templates/custom_insert.js
+++ en-US/templates/custom_insert.js
@@ -1,20 +1,22 @@
 class CustomInsert extends HTMLElement {
     constructor() {
         super();
-        let templateURL = `/en-US/templates/${this.getAttribute("template")}.txt`;
-        function loadXMLDoc() {
-            let xmlhttp;
-            xmlhttp = new XMLHttpRequest();
 
-            xmlhttp.onreadystatechange = function () {
-                if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                    this.innerHTML = xmlhttp.responseText;
-                }
-            }.bind(this); 
+        let templateURL = `/en-US/templates/${this.getAttribute("template")}.htm`;
+        let self = this; 
 
-            xmlhttp.open("GET", templateURL, true);
-            xmlhttp.send();
-        }
-        loadXMLDoc(templateURL);
+        let xmlhttp = new XMLHttpRequest();
+        
+        xmlhttp.open("GET", templateURL, true);
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+                self.outerHTML = xmlhttp.responseText;
+            }
+        };
+        
+        xmlhttp.send();
     }
 }
+
+customElements.define("custom-insert", CustomInsert);
+
