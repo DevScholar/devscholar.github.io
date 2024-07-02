9244f96a5159cba53b43d57d73831492a9621585
DevScholar <88761139+DevScholar@users.noreply.github.com>
2024-07-02 11:14:28 +0800

    add Simplified Chinese support to the website

---
+++ zh-CN/templates/custom_insert.js
@@ -0,0 +1,22 @@
+class CustomInsert extends HTMLElement {
+    constructor() {
+        super();
+
+        let templateURL = `/zh-CN/templates/${this.getAttribute("template")}.htm`;
+        let self = this; 
+
+        let xmlhttp = new XMLHttpRequest();
+        
+        xmlhttp.open("GET", templateURL, true);
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+                self.outerHTML = xmlhttp.responseText;
+            }
+        };
+        
+        xmlhttp.send();
+    }
+}
+
+customElements.define("custom-insert", CustomInsert);
+
